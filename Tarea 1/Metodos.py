9d869557b31e5b01c9a6ea81542a221372b96aaa
Tato2203 <99569490+Tato2203@users.noreply.github.com>
2022-02-18 10:58:50 -0600

    Actualizacion metodos
    se modifico la funcion de num to str

--- Tarea 1/Metodos.py
+++ Tarea 1/Metodos.py
@@ -14,20 +14,24 @@ def string_work(par1):
     
 
 def num_to_str (par2):
-   if type(par2)!= int: #comprobación tipo de entrada      
+   print(par2)
+   if type(par2)!= int: #comprobación tipo de entrada  
+      print("error. 422")    
       return "error. 422"
-      print("error. 422")
+
    else:
-      if par2 <= 99: #comprobación rango de entrada
-         if par2 >=0:
-            letras= num2words(par2, lang='es')#uso de librería para leer y reescribir el número
-            letras=letras.replace(' ','_') #aquí inserta el guión                     
-            return letras
-            print(letras)
-         else: 
-            return "error. 500"
-            print("error. 500")
+      if par2 < 0:#comprobación rango de entrada
+         NRD = "error. 500"
+         print(NRD)
+         return NRD
+      
+      if par2 >= 100:#comprobación rango de entrada
+         NRD2 = "error. 501"
+         print(NRD2)
+         return NRD2
+
       else:
-         return "error 500"
-         print("error. 500") 
-        
+         letras= num2words(par2, lang='es')#uso de librería para leer y reescribir el número
+         letras=letras.replace(' ','_') #aquí inserta el guión                     
+         print(letras)
+         return letras      
