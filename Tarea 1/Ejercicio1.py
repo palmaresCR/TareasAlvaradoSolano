b497de83b1ceb5972e181c8a23538d48c0507666
palmaresCR <73441272+palmaresCR@users.noreply.github.com>
2022-02-17 21:32:27 -0600

    Actualización Ejercicio 1
    Se corrigió validaciones y se comentó el código

--- Tarea 1/Ejercicio1.py
+++ Tarea 1/Ejercicio1.py
@@ -1,6 +1,15 @@
-def string_work(par1):
-    if type(par1)!= str:
-        print("error. 420")
-    else: 
-        print(par1.swapcase())
-        
+#import pytest
+#import row_to_list
+
+#def test_for_clean_row():
+#   assert row_to_list() == \ []
+
+
+def string_work(par1): 
+    if type(par1)!= str: #validacion tipo str
+        print("error. 420")                    
+    else:
+        if (par1.isalpha()): #validación simbolo y caracter
+            print(par1.swapcase()) #Invierte mayusculas y minusculas
+        else:
+            print("error. 420")
