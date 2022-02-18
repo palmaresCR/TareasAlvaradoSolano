757ecdd54c1e1d3d8d2e17dbe30508bb654f77eb
palmaresCR <73441272+palmaresCR@users.noreply.github.com>
2022-02-18 11:32:38 -0600

    Actualización Metodos
    Código con ambos métodos funcionales y comentados

--- Tarea 1/Metodos.py
+++ Tarea 1/Metodos.py
@@ -1,36 +1,23 @@
 from num2words import num2words
 
 def string_work(par1):
-   if type(par1)!= str:
+   if type(par1)!= str: #comprobacion tipo de entrada
       return "error. 420"
-      print("error. 420")
    else:
-      if (par1.isalpha()):
-         return par1.swapcase()
-         print (par1.swapcase())
+      if (par1.isalpha()): #comprobacion de contenido del string (si son letras)
+         return par1.swapcase() #cambia mayus por minus y viceversa
       else:
          return "error. 420"
-         print("error. 420")
-    
 
 def num_to_str (par2):
-   if type(par2)!= int: #comprobación tipo de entrada  
-      #print("error. 422")    
+   if type(par2)!= int: #comprobacion tipo de entrada     
       return "error. 422"
 
    else:
-      if par2 < 0 or par2 >= 100:#comprobación rango de entrada
+      if par2 < 0 or par2 >= 100:#comprobacion rango de entrada
          NRD = "error. 500"
-         #print(NRD)
          return NRD
-      
-      #if par2 >= 100:#comprobación rango de entrada
-      #   NRD2 = "error. 501"
-      #   print(NRD2)
-      #   return NRD2
-
       else:
-         letras= num2words(par2, lang='es')#uso de librería para leer y reescribir el número
-         letras=letras.replace(' ','_') #aquí inserta el guión                     
-         #print(letras)
-         return letras   
+         letras= num2words(par2, lang='es')#uso de libreria para leer y reescribir el numero
+         letras=letras.replace(' ','_') #aqui inserta el guion                     
+         return letras  
