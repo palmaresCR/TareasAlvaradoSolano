1e40e6490c42b93bb4cc5a47aac73051ffb51c02
palmaresCR <73441272+palmaresCR@users.noreply.github.com>
2022-02-18 18:07:26 -0600

    Versión final Pytest.py
    Se actualiza el código con la versión depurada y con formato correcto luego de aplicación de Flake8.

--- Tarea 1/Pytest.py
+++ Tarea 1/Pytest.py
@@ -1,46 +1,57 @@
 import pytest
 from Metodos import*
 
-#Una prueba que verifica que el método string_work es capaz de cambiar todos los caracteres de a-Z. Verificando tanto mayúsculas como minúsculas.
+
+# Una prueba que verifica que el método string_work es capaz de cambiar todos los caracteres de a-Z. Verificando tanto mayúsculas como minúsculas.
 def test_letras():
-   temp = string_work("AbCdefG")
-   assert temp == ("aBcDEFg")
-   
-#Una prueba que verifica que el método string_work retorna el error correcto si se le pasa un número como entrada.
+    temp = string_work("AbCdefG")
+    assert temp == ("aBcDEFg")
+
+# Una prueba que verifica que el método string_work retorna el error correcto si se le pasa un número como entrada.
+
+
 def test_numeros():
-   temp2 = string_work("420")
-   assert temp2 == "error. 420"
-   
-#Una prueba que verifica que el método string_work retorna el error correcto si se le pasa un string con números y/o símbolos.
+    temp2 = string_work("420")
+    assert temp2 == "error. 420"
+
+# Una prueba que verifica que el método string_work retorna el error correcto si se le pasa un string con números y/o símbolos.
+
+
 def test_simbolos():
-   temp3 = string_work("@")
-   assert temp3 == "error. 420"
+    temp3 = string_work("@")
+    assert temp3 == "error. 420"
 
 
-   
-#Una prueba que verifica que el método num_to_str puede traducir todos los números desde el 0 al 99 correctamente.
+# Una prueba que verifica que el método num_to_str puede traducir todos los números desde el 0 al 99 correctamente.
 def test_traducir():
-   temp4 = num_to_str(54)
-   assert temp4 == "cincuenta_y_cuatro"
-   
-def test_traducir1():
-   temp5 = num_to_str(99)
-   assert temp5 == "noventa_y_nueve"
-   
-#Una prueba que verifica que el método num_to_str retorna el error correcto si se le pasa un string.
-def test_string():
-   temp6 = num_to_str("string")
-   assert temp6 == "error. 422"
+    temp4 = num_to_str(54)
+    assert temp4 == "cincuenta_y_cuatro"
 
-#Una prueba que verifica que el método num_to_str retorna el error correcto si se le pasa un número negativo, deciaml o mayor a 99.
+
+def test_traducir1():
+    temp5 = num_to_str(99)
+    assert temp5 == "noventa_y_nueve"
+
+# Una prueba que verifica que el método num_to_str retorna el error correcto si se le pasa un string.
+
+
+def test_string():
+    temp6 = num_to_str("string")
+    assert temp6 == "error. 422"
+
+# Una prueba que verifica que el método num_to_str retorna el error correcto si se le pasa un número negativo, deciaml o mayor a 99.
+
+
 def test_numeros_negativos():
-   temp7 = num_to_str(-99)
-   assert temp7 == ("error. 500")
+    temp7 = num_to_str(-99)
+    assert temp7 == ("error. 500")
+
 
 def test_numeros_mayores():
-   tempx = num_to_str(100)
-   assert tempx == ("error. 500")
+    tempx = num_to_str(100)
+    assert tempx == ("error. 500")
+
 
 def test_numeros_decimales():
-   temp8 = num_to_str(54.9)
-   assert temp8 == ("error. 422")
+    temp8 = num_to_str(54.9)
+    assert temp8 == ("error. 422")
